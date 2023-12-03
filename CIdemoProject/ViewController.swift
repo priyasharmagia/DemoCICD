b973e1f0b212ed50e307184084968f73e3d8997b
Priya Sharma <prisharm@gia.edu>
2023-12-03 20:08:43 +0530

    Initial Commit

---
+++ CIdemoProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CIdemoProject
+//
+//  Created by Priya Sharma on 02/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextFiled: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func submitButtonAction(_ sender: UIButton) {
+        
+    }
+
+
+}
+
