d6bbe431a6b349b491a10cee189ef162f2a15cf1
Priya Sharma <prisharm@gia.edu>
2023-12-07 15:57:00 +0530

    update req

--- CIdemoProject/Controllers/WelcomeVC.swift
+++ CIdemoProject/Controllers/WelcomeVC.swift
@@ -20,7 +20,7 @@ class WelcomeVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        welcomeLabel.text = "Hello \(name), how are you ? updationg jhdvahjgbsa testing somethinngnfbsjfbsjhfgdjhgfdjhgfjdxyfgdjfgdjfgdsjfgdjufgdufghdukfgdghxjgxjkvbdxjvbxj"
+        welcomeLabel.text = "Hello \(name), how are you ? updationg jhdvahjgbsa testing fcghgfdxszerfcxdfghb"
     }
     
 }
